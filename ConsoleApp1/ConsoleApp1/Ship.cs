6806d2891068161e0ff8468c85fe975c3a22d0e0
KubaV <jakub.vaclavik@gentlecodes.com>
2018-09-29 10:17:34 +0200

    some minor change

--- ConsoleApp1/ConsoleApp1/Ship.cs
+++ ConsoleApp1/ConsoleApp1/Ship.cs
@@ -53,6 +53,8 @@ namespace ConsoleApp1
             if (ShipLives <= 0)
             {
                 TH.ShipDied(this);
+                Console.WriteLine($"Ship {Type} has been sunk.");
+                Console.ReadKey();
             }
 
             return true;
